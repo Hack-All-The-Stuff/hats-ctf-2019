f6d810b1aa3a67e58473c35f42f6ae5145937f0f
Enigmatrix <enigmatrix2000@gmail.com>
2019-09-07 12:25:13 +0800

    add crypto (test with Residues)

--- crypto/Residues/solve.py
+++ crypto/Residues/solve.py
@@ -21,8 +21,10 @@ def crt(mp, mq, p, q):
     s = mp*invert(q,p)*q+mq*invert(p,q)*p
     return s % (p*q)
 
-
-r = process(['python2','./chal.py'])
+if args.REMOTE:
+    r = remote('localhost', 1400)
+else:
+    r = process(['python2','./chal.py'])
 r.recvuntil('Here is the encrypted flag: ')
 c = int(r.recvline().strip(),16)
 cl = [enc(r,2),enc(r,4)]
